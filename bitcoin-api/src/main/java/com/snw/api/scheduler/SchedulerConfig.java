1c9bf27a3c70e89154ebdb0f1c17a3e4182fc8a9
Amir <59132773+amirsnw@users.noreply.github.com>
2023-07-31 16:23:08 +0330

    Repachage modules

--- bitcoin-api/src/main/java/com/snw/api/scheduler/SchedulerConfig.java
+++
@@ -1,9 +0,0 @@
-package com.snw.api.scheduler;
-
-import org.springframework.context.annotation.Configuration;
-import org.springframework.scheduling.annotation.EnableScheduling;
-
-@EnableScheduling
-@Configuration
-public class SchedulerConfig {
-}
